50f33dde678d0cd5f30886fc3546bf951375d8c1
RP1930pla <raul.plaruiz@gmail.com>
2023-11-30 23:44:05 +0100

    InitialWork

---
+++ Retro Modern Render Pipeline/Assets/RMRP/Runtime/RMRPAsset.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[CreateAssetMenu(menuName = "Rendering/R-M Render Pipeline")]
+public class RMRPAsset : RenderPipelineAsset
+{
+    protected override RenderPipeline CreatePipeline()
+    {
+        return null;
+    }
+
+
+}
