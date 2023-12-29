ad5669e5bd12f7cec02d8a148ee9d0c028db51b0
RP1930pla <raul.plaruiz@gmail.com>
2023-12-29 15:08:17 +0100

    Alpha options on Unlit shader

--- Retro Modern Render Pipeline/Assets/RMRP/Shaders/UnlitPass.hlsl
+++ Retro Modern Render Pipeline/Assets/RMRP/Shaders/UnlitPass.hlsl
@@ -13,13 +13,19 @@ struct Input
 struct Output
 {
     float4 posCS : SV_POSITION;
+    float2 uv : TEXCOORD0;
     UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
 UNITY_INSTANCING_BUFFER_START(UnityPerMaterial)
+    UNITY_DEFINE_INSTANCED_PROP(float4, _BaseMap_ST)
     UNITY_DEFINE_INSTANCED_PROP(float4,_BaseColor)
+    UNITY_DEFINE_INSTANCED_PROP(float,_Cutoff)
 UNITY_INSTANCING_BUFFER_END(UnityPerMaterial)
 #define IMPS(property) UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, property)
+
+TEXTURE2D(_BaseMap);
+SAMPLER(sampler_BaseMap);
 
 Output UnlitPassVertex(Input i)
 {
@@ -30,13 +36,19 @@ Output UnlitPassVertex(Input i)
 
     float3 posWS = TransformObjectToWorld(i.position.xyz);
     o.posCS = TransformWorldToHClip(posWS);
+
+    o.uv = i.uv * IMPS(_BaseMap_ST).xy + IMPS(_BaseMap_ST).zw;
     return o;
 }
 
 float4 UnlitPassFragment(Output i) : SV_TARGET
 {
     UNITY_SETUP_INSTANCE_ID(i);
-    return IMPS(_BaseColor);
+    float4 baseTexture = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, i.uv);
+    #if defined(_CLIPPING)
+        clip(baseTexture * IMPS(_BaseColor).a - IMPS(_Cutoff));
+    #endif
+    return baseTexture * IMPS(_BaseColor);
 }
 
 
